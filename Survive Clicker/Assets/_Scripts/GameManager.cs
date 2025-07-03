0b62351d8bfc958e08d4d9ed692a4b732ceaad49
Ivan Ljutak <icoljutak@gmail.com>
2025-07-03 22:29:42 +0200

    Created the project and menus

---
+++ Survive Clicker/Assets/_Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private GameObject mainMenuPanel;
+    [SerializeField] private GameObject optionsPanel;
+    [SerializeField] private GameObject pausePanel;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && !mainMenuPanel.activeSelf && !optionsPanel.activeSelf)
+        {
+            PauseGame();
+        }
+
+    }
+
+    public void PlayGame()
+    {
+        mainMenuPanel.SetActive(false);
+    }
+
+    public void OptionsMenu()
+    {
+        optionsPanel.SetActive(true);
+        mainMenuPanel.SetActive(false);
+    }
+
+    public void BackToMainMenu() 
+    {
+        mainMenuPanel.SetActive(true);
+        optionsPanel.SetActive(false);
+    }
+
+    public void PauseGame()
+    {
+        if (Time.timeScale == 1)
+        {
+            Time.timeScale = 0;
+            pausePanel.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1;
+            pausePanel.SetActive(false);
+        }
+    }
+
+}
